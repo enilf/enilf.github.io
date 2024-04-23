339484db56570531ef0a621eeb99aa2b958791fc
Emil Flodmark <emil.flodmark@gmail.com>
2024-04-23 10:27:28 +0200

    23/04

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ function init() {
     lcd = document.getElementById('lcd');
     let keyBoard = document.getElementById('keyBoard')
     keyBoard.onclick = buttonClick;
+    let button = document.getElementsByTagName("button");
 }
 
 /**
@@ -22,12 +23,25 @@ function buttonClick(e) {
     let btn = e.target.id; //id för den tangent som tryckte ner
 
 
-    // kollar om siffertangent är nedtryckt
+    // kollar om sifferknapp är nedtryckt
     if (btn.substring(0, 1) === 'b') {
         let digit = btn.substring(1, 2); // plockar ut siffran från id:et
-
-    } else { // Inte en siffertangent, övriga tangenter.
-
+        addDigit(digit);
+    } 
+    else if (btn === 'clear') {
+        clearLCD();
+        whiteBackground();
+    } 
+    else if (btn === 'enter') {
+        calculate();
+        whiteBackground();
+    }
+    else if (btn === 'comma') {
+        addComma('.');
+    }
+    else {
+        setOperator(btn, lcd.value);
+        backGround(btn);
     }
 }
 
@@ -35,12 +49,20 @@ function buttonClick(e) {
  *  Lägger till siffra på display.
  */
 function addDigit(digit) {
+    lcd.value = lcd.value + digit;
 }
 
 /**
  * Lägger till decimaltecken
  */
-function addComma() {
+function addComma(comma) {
+   
+    if(lcd.value.includes('.')){
+        console.log('1')
+    } 
+    else{
+        lcd.value = lcd.value + comma;
+    }
 
 }
 
@@ -48,14 +70,43 @@ function addComma() {
  * Sparar operator.
  * +, -, *, /
  */
-function setOperator(operator){
-
+function setOperator(operator, siffra) { 
+    arithmetic = operator;
+    memory = siffra;
+    clearLCD();
 }
 
 /**
- * Beräknar ovh visar resultatet på displayen.
+ * Beräknar och visar resultatet på displayen.
  */
 function calculate() {
+    
+    let decAmount = 0;
+    console.log(memory);
+
+    /* Kollar antal decimaler */
+    while (lcd.value % 1 != 0 || memory % 1 != 0) {
+        lcd.value *= 10;
+        memory *= 10;
+        decAmount++;
+    }
+
+    console.log(lcd.value);
+    console.log(memory);
+
+
+    if(arithmetic === 'add'){
+        lcd.value = (+memory + +lcd.value) / Math.pow(10, decAmount);
+    }   
+    else if(arithmetic === 'sub'){
+        lcd.value = (memory - lcd.value) / Math.pow(10, decAmount);
+    }
+    else if(arithmetic === 'mul'){
+        lcd.value = (memory * lcd.value) / Math.pow(10, 2 * decAmount);
+    }
+    else if(arithmetic === 'div'){
+        lcd.value = memory / lcd.value;
+    }
 
 }
 
@@ -66,10 +117,34 @@ function clearLCD() {
 }
 
 /** Rensar allt, reset */
-function memClear(){
+function memClear() {
     memory = 0;
     arithmetic = null;
     clearLCD();
+}
+
+function backGround(operator){
+    whiteBackground();
+
+    if(operator === "add") {
+        document.getElementById("add").style.backgroundColor = "lightgray"
+    }
+    else if(operator === "sub") {
+        document.getElementById("sub").style.backgroundColor = "lightgray"
+    }
+    else if(operator === "mul") {
+        document.getElementById("mul").style.backgroundColor = "lightgray"
+    }
+    else if(operator === "div") {
+        document.getElementById("div").style.backgroundColor = "lightgray"
+    }
+}
+
+function whiteBackground(){
+    document.getElementById("add").style.backgroundColor = "white"
+    document.getElementById("sub").style.backgroundColor = "white"
+    document.getElementById("mul").style.backgroundColor = "white"
+    document.getElementById("div").style.backgroundColor = "white"
 }
 
 window.onload = init;
